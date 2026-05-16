fbd9cfab623a77a97a71306d26a9a89fb11eccc3
tanaji barkade <tanajeebarkade27@gmail.com>
2026-05-16 12:35:02 +0530

    video servilance

---
+++ TuyaDevices/LockView/VideoSurveillanceVc.swift
@@ -0,0 +1,29 @@
+//
+//  VideoSurveillanceVc.swift
+//  SkromanIsra
+//
+//  Created by Admin on 15/05/26.
+//
+
+import UIKit
+
+class VideoSurveillanceVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
